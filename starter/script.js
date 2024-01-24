14309315ba1aa5dd2d8b8c0c166520f3d9b24d27
sernamalala <sernaprograms@gmail.com>
2024-01-24 14:24:45 +0200

    basic change of user message when value entered is zero

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+//DOM MANIPULATION
+const message = document.querySelector(".message");
+console.log(message)
+
+const score = document.querySelector(".score");
+const number = document.querySelector(".number");
+
+score.textContent = 13;
+number.textContent = 20;
+
+const guess = Number(document.querySelector(".guess").value);
+
+//EVENT LISTENER
+
+const checkButton = document.querySelector(".check");
+checkButton.addEventListener("click",function(){
+    const guessValueFromUser = Number(document.querySelector(".guess").value);
+    console.log(guessValueFromUser, typeof guess);
+
+    //evaluation
+
+    if(!guess){
+        message.textContent = "❌ Not a number";
+    }
+});
